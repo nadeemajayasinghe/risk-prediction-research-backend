f6d46b538b902563ebcb99fff3765e4738b46a22
nadeemajayasinghe <nadeemajayasinghe75@gmail.com>
2026-05-06 00:27:13 +0530

    Add initial project structure with Maven configuration and Spring Boot setup

---
+++ src/test/java/com/project/management/risk/prediction/system/RiskPredictionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.management.risk.prediction.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskPredictionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
