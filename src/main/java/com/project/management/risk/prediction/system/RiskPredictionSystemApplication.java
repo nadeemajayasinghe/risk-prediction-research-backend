9a9f1c8f617774e3da75442950e395f7a1c4748b
nadeemajayasinghe <nadeemajayasinghe75@gmail.com>
2026-05-06 00:27:13 +0530

    Add initial project structure with Maven configuration and Spring Boot setup

---
+++ src/main/java/com/project/management/risk/prediction/system/RiskPredictionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.management.risk.prediction.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskPredictionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskPredictionSystemApplication.class, args);
+	}
+
+}
